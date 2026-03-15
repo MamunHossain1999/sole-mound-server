01444e83930c97702e20027f9b8263de9e6b54b0
Your Name <youremail@example.com>
2026-03-15 23:00:03 +0600

    global error handle

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+
+  res.status(status).json({
+    success: false,
+    message,
+  });
+};
