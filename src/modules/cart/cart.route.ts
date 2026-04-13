dccf6f8c4830494921ecb02b70ea7d4f6e4dfe94
Your Name <youremail@example.com>
2026-04-13 09:31:44 +0600

    add cart route

---
+++ src/modules/cart/cart.route.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { protect } from "../../middlewares/auth.middleware";
+import { addToCart, clearCart, getCart, removeFromCart, updateCartQuantity } from "./cart.controller";
+import { authorize } from "../../middlewares/role.middleware";
+
+
+
+const router = Router();
+
+// Wishlist
+
+
+// Cart
+router.post("/cart", protect, authorize("user", "seller", "admin"), addToCart);
+router.get("/cart", protect, authorize("user", "seller", "admin"), getCart);
+router.put("/cart/update", protect, authorize("user", "seller", "admin"), updateCartQuantity);
+router.delete("/single/cart/:productId", protect, authorize("user", "seller", "admin"), removeFromCart);
+
+router.delete("/cart/clear", protect, authorize("user", "seller", "admin"), clearCart);
+const cartRoutes = router;
+export default cartRoutes;
