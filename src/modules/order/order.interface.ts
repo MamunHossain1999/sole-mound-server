05c4648fbca16ccddbc97139cf8cad93ef80b7a0
Your Name <youremail@example.com>
2026-04-16 22:44:59 +0600

    order interface

---
+++ src/modules/order/order.interface.ts
@@ -0,0 +1,56 @@
+import { Types } from "mongoose";
+
+export interface IOrderProduct {
+  productId: string;
+  name?: string;
+  image?: string;
+  quantity: number;
+  price: number;
+}
+
+export interface IShippingAddress {
+  fullName: string;
+  phone: string;
+  address: string;
+  city?: string;
+  postalCode?: string;
+  country?: string;
+}
+export interface IBillingAddress {
+  fullName: string;
+  phone: string;
+  address: string;
+  city?: string;
+  postalCode?: string;
+  country?: string;
+}
+
+export type OrderStatus = "pending" | "processing" | "completed" | "cancelled";
+
+export type PaymentStatus = "unpaid" | "paid" | "failed";
+
+export type PaymentMethod = "stripe" | "sslcommerz" | "cash";
+
+export interface IOrder {
+  _id?: string;
+
+  userId: Types.ObjectId | string; // ✅ FIX
+
+  products: IOrderProduct[];
+
+  totalAmount: number;
+
+  paymentStatus: PaymentStatus;
+
+  status: OrderStatus;
+
+  shippingAddress: IShippingAddress;
+  billingAddress?: IBillingAddress;
+
+  transactionId?: string | null;
+
+  paymentMethod?: PaymentMethod;
+
+  createdAt?: string; // ✅ better for frontend
+  updatedAt?: string; // ✅ better for frontend
+}
